f635165c10bed95f23ba83f19c1d29fadd89df48
LiXiaRui <1015408596@qq.com>
2019-12-04 21:18:52 +0800

    实现地图上表格显示

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -2,122 +2,123 @@
   <div class="hello">
     <el-container>
       <el-header>
-        <el-menu router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-submenu index="shipInformation" @click.native="goTo('/shipInformation')">
-            <template slot="title">渔船基本信息</template>
-            <el-submenu index="2-1">
-              <template slot="title">统计分析</template>
-              <el-menu-item index="2-1-1" >渔船类型统计</el-menu-item>
-              <el-menu-item index="2-1-2">渔船归属统计</el-menu-item>
-              <el-menu-item index="2-1-3">在港离港统计</el-menu-item>
-            </el-submenu>
-            <el-submenu index="2-2">
-              <template slot="title">统计分析</template>
-              <el-menu-item index="2-2-1">渔船信息检索</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">渔船船员信息</template>
-            <el-submenu index="3-1">
-              <template slot="title">统计分析</template>
-              <el-menu-item index="3-1-1">年龄统计</el-menu-item>
-              <el-menu-item index="3-1-2">性别统计</el-menu-item>
-              <el-menu-item index="3-1-3">健康统计</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3-2">
-              <template slot="title">基本查询</template>
-              <el-menu-item index="3-2-1">等级状态查询</el-menu-item>
-              <el-menu-item index="3-2-1">持证状态查询</el-menu-item>
-              <el-menu-item index="3-2-1">人员信息检索</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">应急指挥与安全救助</template>
-            <el-menu-item index="4-1-1">海上应急救援</el-menu-item>
-            <el-menu-item index="4-1-2">海上联络通讯</el-menu-item>
-            <el-menu-item index="4-1-3">防台防汛指挥</el-menu-item>
-            <el-menu-item index="4-1-4">渔业事故复现</el-menu-item>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">渔业资源管理</template>
-            <el-submenu index="5-1">
-              <template slot="title">区域划分管理</template>
-              <el-menu-item index="5-1-1">休闲渔船作业区</el-menu-item>
-              <el-menu-item index="5-1-2">资源保护区</el-menu-item>
-              <el-menu-item index="5-1-3">养殖区</el-menu-item>
-              <el-menu-item index="5-1-4">海钓区</el-menu-item>
-            </el-submenu>
-            <el-menu-item index="3-2-1">区域监控管理</el-menu-item>
-            <el-submenu index="5-3">
-              <template slot="title">渔业捕捞日志管理及统计</template>
-              <el-menu-item index="5-3-1">养殖区统计</el-menu-item>
-              <el-menu-item index="5-3-2">捕捞统计</el-menu-item>
-              <el-menu-item index="5-3-3">其他</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="6">
-            <template slot="title">渔业和海监执法管理</template>
-            <el-menu-item index="6-1">渔港码头检查处置</el-menu-item>
-            <el-menu-item index="6-2">用海检查处置</el-menu-item>
-            <el-menu-item index="6-3">海上执法处置</el-menu-item>
-            <el-submenu index="6-4">
-              <template slot="title">执法监管成果展示</template>
-              <el-menu-item index="6-4-1">保护区</el-menu-item>
-              <el-menu-item index="6-4-2">禁渔区</el-menu-item>
-              <el-menu-item index="6-4-3">其他等</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="7">
-            <template slot="title">养殖与水产品质量监管</template>
-            <el-menu-item index="7-1">养殖场管理</el-menu-item>
-            <el-menu-item index="7-2">水产坪质量检查</el-menu-item>
-            <el-menu-item index="7-3">用药管理</el-menu-item>
-            <el-submenu index="7-4">
-              <template slot="title">信息统计</template>
-              <el-menu-item index="7-4-1">产量统计</el-menu-item>
-              <el-menu-item index="7-4-2">质量统计</el-menu-item>
-              <el-menu-item index="7-4-3">其他</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="8">
-            <template slot="title">远洋渔业信息图</template>
-            <el-menu-item index="8-1">资源分布展示</el-menu-item>
-            <el-menu-item index="8-2">远洋渔船位置</el-menu-item>
-            <el-menu-item index="8-3">数据交换管理</el-menu-item>
-          </el-submenu>
-          <el-submenu index="9">
-            <template slot="title">渔港监管服务</template>
-            <el-menu-item index="9-1">雷达管理</el-menu-item>
-            <el-menu-item index="9-2">监控管理</el-menu-item>
-            <el-menu-item index="9-3">人员管理</el-menu-item>
-            <el-menu-item index="9-4">渔港设施管理</el-menu-item>
-            <el-menu-item index="9-5">港区环境管理</el-menu-item>
-            <el-menu-item index="9-6">渔港建设管理</el-menu-item>
-            <el-menu-item index="9-7">信息统计</el-menu-item>
-          </el-submenu>
-          <el-submenu index="10">
-            <template slot="title">渔业经济信息</template>
-            <el-menu-item index="9-1">渔业数据统计及报送</el-menu-item>
-            <el-menu-item index="9-2">渔业产业规划布局管理</el-menu-item>
-            <el-menu-item index="9-3">渔港经济区建设与管理</el-menu-item>
-            <el-menu-item index="9-4">渔业增效渔民增收统计</el-menu-item>
-          </el-submenu>
-          <el-submenu index="11">
-            <template slot="title">对外公众服务</template>
-            <el-menu-item index="7-1">渔船动态展示</el-menu-item>
-            <el-menu-item index="7-2">气象信息展示</el-menu-item>
-            <el-menu-item index="7-3">休闲作业区域展示</el-menu-item>
-            <el-menu-item index="7-3">海钓场展示</el-menu-item>
-            <el-menu-item index="7-3">海洋水文展示</el-menu-item>
-            <el-submenu index="7-4">
-              <template slot="title">监管举报</template>
-              <el-menu-item index="7-4-1">伏休期举报</el-menu-item>
-              <el-menu-item index="7-4-2">非法捕捞举报</el-menu-item>
-              <el-menu-item index="7-4-3">涉渔三无船只举报</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-        </el-menu>
-        <div class="line"></div>
+
+               <el-menu router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-submenu index="shipInformation" @click.native="goTo('/shipInformation')">
+                  <template slot="title">渔船基本信息</template>
+                  <el-submenu index="2-1">
+                    <template slot="title">统计分析</template>
+                    <el-menu-item index="shipInformation" @click.native="goTo('/shipInformation')" >渔船类型统计</el-menu-item>
+                    <el-menu-item index="2-1-2">渔船归属统计</el-menu-item>
+                    <el-menu-item index="2-1-3">在港离港统计</el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="2-2">
+                    <template slot="title">统计分析</template>
+                    <el-menu-item index="2-2-1">渔船信息检索</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-submenu index="3">
+                  <template slot="title">渔船船员信息</template>
+                  <el-submenu index="3-1">
+                    <template slot="title">统计分析</template>
+                    <el-menu-item index="3-1-1">年龄统计</el-menu-item>
+                    <el-menu-item index="3-1-2">性别统计</el-menu-item>
+                    <el-menu-item index="3-1-3">健康统计</el-menu-item>
+                  </el-submenu>
+                  <el-submenu index="3-2">
+                    <template slot="title">基本查询</template>
+                    <el-menu-item index="3-2-1">等级状态查询</el-menu-item>
+                    <el-menu-item index="3-2-1">持证状态查询</el-menu-item>
+                    <el-menu-item index="3-2-1">人员信息检索</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-submenu index="4">
+                  <template slot="title">应急指挥与安全救助</template>
+                  <el-menu-item index="4-1-1">海上应急救援</el-menu-item>
+                  <el-menu-item index="4-1-2">海上联络通讯</el-menu-item>
+                  <el-menu-item index="4-1-3">防台防汛指挥</el-menu-item>
+                  <el-menu-item index="4-1-4">渔业事故复现</el-menu-item>
+                </el-submenu>
+                <el-submenu index="5">
+                  <template slot="title">渔业资源管理</template>
+                  <el-submenu index="5-1">
+                    <template slot="title">区域划分管理</template>
+                    <el-menu-item index="5-1-1">休闲渔船作业区</el-menu-item>
+                    <el-menu-item index="5-1-2">资源保护区</el-menu-item>
+                    <el-menu-item index="5-1-3">养殖区</el-menu-item>
+                    <el-menu-item index="5-1-4">海钓区</el-menu-item>
+                  </el-submenu>
+                  <el-menu-item index="3-2-1">区域监控管理</el-menu-item>
+                  <el-submenu index="5-3">
+                    <template slot="title">渔业捕捞日志管理及统计</template>
+                    <el-menu-item index="5-3-1">养殖区统计</el-menu-item>
+                    <el-menu-item index="5-3-2">捕捞统计</el-menu-item>
+                    <el-menu-item index="5-3-3">其他</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-submenu index="6">
+                  <template slot="title">渔业和海监执法管理</template>
+                  <el-menu-item index="6-1">渔港码头检查处置</el-menu-item>
+                  <el-menu-item index="6-2">用海检查处置</el-menu-item>
+                  <el-menu-item index="6-3">海上执法处置</el-menu-item>
+                  <el-submenu index="6-4">
+                    <template slot="title">执法监管成果展示</template>
+                    <el-menu-item index="6-4-1">保护区</el-menu-item>
+                    <el-menu-item index="6-4-2">禁渔区</el-menu-item>
+                    <el-menu-item index="6-4-3">其他等</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-submenu index="7">
+                  <template slot="title">养殖与水产品质量监管</template>
+                  <el-menu-item index="7-1">养殖场管理</el-menu-item>
+                  <el-menu-item index="7-2">水产坪质量检查</el-menu-item>
+                  <el-menu-item index="7-3">用药管理</el-menu-item>
+                  <el-submenu index="7-4">
+                    <template slot="title">信息统计</template>
+                    <el-menu-item index="7-4-1">产量统计</el-menu-item>
+                    <el-menu-item index="7-4-2">质量统计</el-menu-item>
+                    <el-menu-item index="7-4-3">其他</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-submenu index="8">
+                  <template slot="title">远洋渔业信息图</template>
+                  <el-menu-item index="8-1">资源分布展示</el-menu-item>
+                  <el-menu-item index="8-2">远洋渔船位置</el-menu-item>
+                  <el-menu-item index="8-3">数据交换管理</el-menu-item>
+                </el-submenu>
+                <el-submenu index="9">
+                  <template slot="title">渔港监管服务</template>
+                  <el-menu-item index="9-1">雷达管理</el-menu-item>
+                  <el-menu-item index="9-2">监控管理</el-menu-item>
+                  <el-menu-item index="9-3">人员管理</el-menu-item>
+                  <el-menu-item index="9-4">渔港设施管理</el-menu-item>
+                  <el-menu-item index="9-5">港区环境管理</el-menu-item>
+                  <el-menu-item index="9-6">渔港建设管理</el-menu-item>
+                  <el-menu-item index="9-7">信息统计</el-menu-item>
+                </el-submenu>
+                <el-submenu index="10">
+                  <template slot="title">渔业经济信息</template>
+                  <el-menu-item index="9-1">渔业数据统计及报送</el-menu-item>
+                  <el-menu-item index="9-2">渔业产业规划布局管理</el-menu-item>
+                  <el-menu-item index="9-3">渔港经济区建设与管理</el-menu-item>
+                  <el-menu-item index="9-4">渔业增效渔民增收统计</el-menu-item>
+                </el-submenu>
+                <el-submenu index="11">
+                  <template slot="title">对外公众服务</template>
+                  <el-menu-item index="11-1">渔船动态展示</el-menu-item>
+                  <el-menu-item index="11-2">气象信息展示</el-menu-item>
+                  <el-menu-item index="11-3">休闲作业区域展示</el-menu-item>
+                  <el-menu-item index="11-4">海钓场展示</el-menu-item>
+                  <el-menu-item index="11-5">海洋水文展示</el-menu-item>
+                  <el-submenu index="11-4">
+                    <template slot="title">监管举报</template>
+                    <el-menu-item index="11-4-1">伏休期举报</el-menu-item>
+                    <el-menu-item index="11-4-2">非法捕捞举报</el-menu-item>
+                    <el-menu-item index="11-4-3">涉渔三无船只举报</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+              </el-menu>
+              <div class="line"></div>         
       </el-header>
       <el-main>
         <router-view></router-view>
@@ -160,6 +161,8 @@ export default {
     color: #333;
     text-align: center;
     line-height: 60px;
+    padding: 0;
+    height: 60px;
   }
   
   .el-aside {
@@ -173,7 +176,8 @@ export default {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-    line-height: 160px;
+    // line-height: 160px;
+    padding: 0;
   }
   
   body > .el-container {
@@ -188,4 +192,23 @@ export default {
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
   }
+  .el-menu{
+    width: 100%;
+    height: 60px;
+  }
+  .el-menu[data-v-67f3a69c]{
+    padding-top: 10px;
+
+    height:70px;
+
+  }
+
+
+</style>
+
+<style>
+.el-menu--horizontal>.el-submenu .el-submenu__title{
+  padding: 0 10px;
+  font-size: 16px;
+}
 </style>
--- src/components/ship-information/ship-information.vue
+++ src/components/ship-information/ship-information.vue
@@ -1,6 +1,95 @@
 <template>
   <div>
-    <div id="allmap" ref="allmap"></div>
+    <div id="allmap" ref="allmap">   
+    </div>
+    <div class="detailMenu">
+      <div class="search">
+        <div style="margin-top: 15px;">
+          <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+            <el-select v-model="select" slot="prepend" placeholder="请选择">
+              <el-option label="远洋船" value="1"></el-option>
+              <el-option label="国内船" value="2"></el-option>
+              <el-option label="休闲船" value="3"></el-option>
+            </el-select>
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+      </div>
+      <div class="shipType">
+        <el-table
+          :data="tableData"          
+          style="width: 100%;margin-bottom: 20px;font-size:14px;Opacity:0.8;"
+          row-key="id"
+          border
+          default-expand-all
+          :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+          <el-table-column 
+            prop="type"           
+            label="渔船信息"
+            width="143">
+            <template slot-scope="scope">
+              <el-button :disabled="scope.row.type != '船1'" @click="show = true" type="text" size="small">{{scope.row.type}}</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+    <div class="shipDetail">
+      <el-table
+        :data="shipData"
+        v-if="show" 
+        style="width: 100%;margin-bottom: 20px;font-size:14px;Opacity:0.7;"
+        row-key="id"
+        border
+        default-expand-all
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+        <el-table-column         
+          label="船东"
+          width="130"> 
+          <template slot-scope="scope">
+            <el-button @click="shipershow = true" type="text" size="small">{{scope.row.shiplord}}</el-button>
+          </template>          
+        </el-table-column>
+        <el-table-column
+          prop="shipstuff"
+          label="船员"
+          width="130">
+        </el-table-column>
+        <el-table-column
+          prop="shipstatus"
+          label="渔船状态"
+          width="135">
+        </el-table-column>
+      </el-table>
+      <div class="shiplord">
+        <el-table
+          :data="shiperData"
+          v-if="shipershow" 
+          style="width: 100%;margin-bottom: 20px;font-size:14px;Opacity:0.7;"
+          row-key="id"
+          border
+          default-expand-all
+          :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+          <el-table-column  
+            prop="shiplord"       
+            label="船东"
+            width="130">   
+          </el-table-column>
+          <el-table-column
+            prop="phone"
+            label="手机号"
+            width="130">
+          </el-table-column>
+          <el-table-column
+            prop="shipnum"
+            label="渔船数量"
+            width="135">
+          </el-table-column>
+        </el-table>
+
+      </div>
+
+    </div>
     <!-- <router-view></router-view> -->
   </div>
 </template>
@@ -20,7 +109,50 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      map: null
+      map: null,
+      input1: '',
+      input2: '',
+      input3: '',
+      select: '',
+      tableData: [{
+          id: 1,
+          type: '远洋渔船',
+        }, {
+          id: 2,
+          type: '休闲渔船',
+        }, {
+          id: 3,
+          type: '国内渔船',
+          children: [{
+              id: 31,
+              type: '船1',
+            }, {
+              id: 32,
+              type: '船2',
+          }, {
+              id: 33,
+              type: '船3',
+          }]
+        }, {
+          id: 4,
+          type: '海钓渔船',
+        },
+        {
+          id: 5,
+          type: '执法船展示',
+        }],
+        shipData: [{
+          shiplord: '张三',
+          shipstuff: '王小虎',
+          shipstatus: '在线'
+        }],
+        shiperData: [{
+          shiplord: '张三',
+          phone: '16688556688',
+          shipnum: '20'
+        }],
+        show: false,
+        shipershow: false,
     };
   },
   methods: {
@@ -60,7 +192,15 @@ export default {
           transform(_this.map.getEventCoordinate(event),"EPSG:3857","EPSG:4326")
         );
       });
-    }
+    },
+    handleClick(row) {
+        console.log(row);
+         this.$notify({
+          title: '提示',
+          message: '这是一条不会自动关闭的消息',
+          duration: 0
+        });
+      }
   },
 
   mounted() {
@@ -81,9 +221,99 @@ export default {
 /*设置地图的宽高*/
 #allmap{
  margin:0px; 
- height: 800px;
+ height: 1000px;
  width:100%; 
  overflow: hidden;
  padding: 0px; 
 } 
+.el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  .search{
+    position: relative;
+    width: 500px;
+    height: 20px;
+    // background-color: aqua;
+    float: left;
+  }
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+
+  .shipType{
+    height: 600px;
+    width: 200px;
+    position: relative;
+    margin-top: 200px;
+    float: left;
+  }
+
+ .detailMenu{
+   position: absolute;
+   top:100px;
+   left:50px;
+   width: 500px;
+   height: 600px;
+  //  background-color: #99a9bf;
+ }
+
+.shipDetail{
+  position: absolute;
+  width: 550px;
+  height: 700px;
+  top:400px;
+  left: 70%;
+  // background-color:#99a9bf;
+}
+.shipInfor{
+  position: relative;
+  width: 550px;
+  height: 300px;
+  // background-color:aqua;
+}
+.shiplord{
+  position: relative;
+  width: 550px;
+  height: 300px;
+}
+</style>
+<style scope>
+.el-table__header tr,
+ .el-table__header th
+  { 
+    /* background-color: rgb(216, 231, 231); */
+    font-weight: 800;
+    color: black;
+    text-align:center;
+  }
+.el-table th, .el-table tr {
+background-color: transparent;
+}
+
 </style>
